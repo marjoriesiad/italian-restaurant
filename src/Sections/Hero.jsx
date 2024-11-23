6ecdf26fe30a02c90670c672461ba40d8bee4a4d
Marjorie Siad <marjoriesiad@gmail.com>
2024-11-23 20:09:47 +0100

    refactor: text placement on hero section

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <>
     <div className='flex justify-center'>
       <img src="https://firebasestorage.googleapis.com/v0/b/code-up-31d9f.appspot.com/o/pro-projects-ressources%2Frestaurant%2Fmain-picture.png?alt=media" alt="" className='object-cover h-[517px] w-full absolute' />
-      <div className='relative bg-white lg:w-[513px] w-[312px] h-[120px] flex justify-center items-center top-36 rounded-lg p-4 opacity-70'>
+      <div className='relative text-xl text-center bg-white lg:w-[513px] w-[312px] h-[120px] flex justify-center items-center top-36 rounded-lg p-4 opacity-70'>
         <blockquote className='text-slate-600'>Enjoy our homemade Italian pizzas, prepared with passion and fresh ingredients!</blockquote>
       </div>
     </div>
