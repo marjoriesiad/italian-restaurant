37ab4065dc1a6d639b5597d362452c4ebcaa02e5
Marjorie Siad <marjoriesiad@gmail.com>
2024-11-23 21:59:35 +0100

    build: about-us section / refactor: make navbar fixed on top

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='flex items-center justify-around h-14'>
+    <div className='flex items-center justify-around h-14 fixed bg-white w-full'>
       <div className='flex items-center gap-2'>
         <img src="https://firebasestorage.googleapis.com/v0/b/code-up-31d9f.appspot.com/o/pro-projects-ressources%2Frestaurant%2Flogo.png?alt=media" alt="Logo Pizzeria Bella Luna" className='h-10 w-10' />
         <blockquote className='italic'>Pizzeria Bella Luna</blockquote>
